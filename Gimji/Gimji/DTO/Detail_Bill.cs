e7018c2c59efde409ef22da1be9cbe733549d189
long <thanhlongqax@gmail.com>
2023-10-27 23:52:20 +0700

    thanhlong

---
+++ Gimji/Gimji/DTO/Detail_Bill.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gimji.DTO
+{
+    internal class Detail_Bill
+    {
+        public int Detail_BillID { get; set; }
+        public int Bill_ID { get; set;}
+
+        public string foodID { get; set; }
+
+        public int quanity { get; set; }
+        public float total_Amount { get; set; }
+
+        public int paymentID { get; set; }
+
+
+    }
+}
---
+++ Gimji/Gimji/DTO/Detail_Delivery.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gimji.DTO
+{
+    internal class Detail_Delivery
+    {
+        public int Detail_DeliveryId { get; set; }
+
+        public int DeliveryId { get; set; }
+        public string foodID { get; set; }
+        public int quanity { get; set; }
+        public String DeliveryName { get; set; }
+
+    }
+}
