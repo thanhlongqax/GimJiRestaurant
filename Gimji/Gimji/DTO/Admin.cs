587e4afa2fe2db13ec7b0a09f4b50cc0d19b822e
long <thanhlongqax@gmail.com>
2023-10-31 00:26:47 +0700

    longCommit

---
+++ Gimji/Gimji/DTO/Admin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gimji.DTO
+{
+    internal class Admin
+    {
+        public int AdminId { get; set; }
+        public string AdminName { get; set; }
+        public string AdminPassword { get; set; }
+        public string fullName { get; set; }
+        public string Gender { get; set; }
+        public string phoneNumber { get; set; }
+        public string Email { get; set; }
+
+    }
+}
